4fc6665efd37689ba9a12a31a395c35a11851e63
Stephani6G <stephaniegloria283@gmail.com>
2023-12-03 23:15:06 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+/**
+* struct format_operators - function format checker object
+* @ch: flag
+* @func: function
+*/
+
+typedef struct format_operators
+{
+	char *ch;
+	int (*func)(va_list);
+} f_ops;
+
+
+/*putchar function */
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+int (*check_func(char s))(va_list inputs);
+int print_c(va_list inputs);
+int print_s(va_list inputs);
+int print_d(va_list inputs);
+int print_i(va_list inputs);
+int print_b(va_list inputs);
+
+#endif /* MAIN_H */
