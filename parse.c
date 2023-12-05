688c3529eca6811681837d6f50cf181fb14f193c
Stephani6G <stephaniegloria283@gmail.com>
2023-12-04 23:08:19 -0800

    func.c

--- parse.c
+++ parse.c
@@ -20,18 +20,18 @@ int parser(const char *format, conver_t f_list[], va_list arg_list)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; f_list[j].sym != NULL; j++)
+			for (j = 0; f_list[j].st != NULL; j++)
 			{
-				if (format[i + 1] == f_list[j].sym[0])
+				if (format[i + 1] == f_list[j].st[0])
 				{
-					r_val = f_list[j].f(arg_list);
+					r_val = f_list[j].fn(arg_list);
 					if (r_val == -1)
 						return (-1);
 					printed_char += r_val;
 					break;
 				}
 			}
-			if (f_list[j].sym == NULL && format[i + 1] != ' ')
+			if (f_list[j].st == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
